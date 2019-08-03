1f65e85789b414dfbc331358e5c02ecb7ded60fc
Peachlilo <53659963+Peachlilo@users.noreply.github.com>
2019-08-03 20:09:29 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+print ("print senders", senders)
+
+ {1: 'ggolden22@mac.com', 2: 'csev@umich.edu', 3: 'kevin.carpenter@rsmart.com', 4: 'michael.feldstein@suny.edu', 5: 'john@cam.ac.uk', 6: 'swgithen@mtu.edu', 7: 'ys2n@virginia.edu', 8: 'slt@columbia.edu', 9: 'dgcotton@ucdavis.edu', 10: 'vyu2@yahoo.com', 11: 'winstonw.tw@gmail.com', 12: 'ajpoland@indiana.edu', 13: 'caseyd1@stanford.edu', 14: 'azeckoski@unicon.net', 15: 'j.higham@hull.ac.uk', 16: 'jeffrey.beeman@asu.edu', 17: 'pgoldweic@northwestern.edu', 18: 'andrew@cam.ac.uk', 19: 'john.ellis@rsmart.com', 20: 'jxf@immagic.com', 21: 'ray@berkeley.edu', 22: 'dave.ross@gmail.com', 23: 'clay.fenlason@gatech.edu', 24: 'lfernandez@weber.edu', 25: 'jleasia@umich.edu', 26: 'gsilver@umich.edu', 27: 'jrm@jhu.edu', 28: 'antranig@cam.ac.uk', 29: 'jmpease@syr.edu', 30: 'gwang3@unl.edu', 31: 'bkirschn@umich.edu', 32: 'vrajgopalan@ucmerced.edu', 33: 'wchang@dri.edu', 34: 'jholtzman@berkeley.edu', 35: 'listuser@chillco.com', 36: 'murthy@etudes.org', 37: 'fribeiro@ufp.pt', 38: 'iros@umass.edu', 39: 'masayang.mobile@gmail.com', 40: 'cheryl.wogahn@yale.edu', 41: 'k.cheng@ust.hk', 42: 'jwilliamson@ucla.edu', 43: 'zqian@umich.edu', 44: 'raad@cam.ac.uk', 45: 'gissurj@ruc.dk', 46: 'mak05307@yahoo.com', 47: 'sinou@etudes.org', 48: 'richard.walker@anu.edu.au', 49: 'marquard@uct.ac.za', 50: 'esmiley@stanford.edu', 51: 'nuno@ufp.pt', 52: 'news_tmp1@edegger.com', 53: 'v.n.maijer@uva.nl', 54: 'jimeng@umich.edu', 55: 'amitku@uiuc.edu', 56: 'markjnorton@earthlink.net',
+
+print(messages.items())
+ dict_items([(1, ('<b7eaf76e-3366-4ae3-bdd6-6743707feba9@umich.edu>', 1, 1, '2005-12-09 05:34:30')), 
+ (2, ('<bb7ce631-ccf0-40a3-b076-683f9d03e0c9@umich.edu>', 2, 2, '2005-12-09 05:58:01')),
+(3, ('<16703710.1134144397398.javamail.tomcat5@sharkfin.ds.itd.umich.edu>', 3, 3, '2005-12-09 16:01:49')),
+(4, ('<9622225.1134139564303.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 4, 4, '2005-12-09 14:43:12')),
+(5, ('<22585576.1134135343809.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 5, 4, '2005-12-09 13:32:29')),
+(6, ('<29494755.1134157064976.javamail.tomcat5@sharkfin.ds.itd.umich.edu>', 6, 5, '2005-12-09 19:32:31')),
+(7, ('<20623058.1134135893412.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 5, 4, '2005-12-09 13:42:24')),
+(8, ('<14721465.1134163692331.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 6, 5, '2005-12-09 21:23:09')),
+(9, ('<31872472.1134150954615.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 7, 6, '2005-12-09 17:54:17')),
+(10, ('<1304695.1134163143767.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 7, 5, '2005-12-09 21:16:39')),
+(11, ('<3598962.1134188359277.javamail.tomcat5@sharkfin.ds.itd.umich.edu>', 8, 7, '2005-12-10 04:16:40')),
+(12, ('<590808.1134277104504.javamail.tomcat5@mahimahi.ds.itd.umich.edu>', 9, 8, '2005-12-11 04:55:54')),
+ (10496, ('<346eb1500702230422n4c56f6f8xe239345c2769468e@mail.gmail.com>', 22, 5168, '2007-02-23 12:22:54')),
+(10497, ('<6c20282c0702230643s6c3dd359rffb2fb25c05ba137@mail.gmail.com>', 141, 5169, '2007-02-23 14:43:32')),
+(10498, ('<4f196b82-f5af-4cd1-8aa6-db2fa4abfa14@rice.edu>', 208, 5170, '2007-02-23 13:45:32')),
+(10499, ('<6c20282c0702230812y2f2a86bdm72bdef541a67f7c5@mail.gmail.com>', 141, 5171, '2007-02-23 16:12:54')),
+(10500, ('<6c20282c0702230854l1efe8804h8c573e24a7b18786@mail.gmail.com>', 141, 5172, '2007-02-23 16:54:35')),
+(10501, ('<feb4564e-89db-4b3f-af06-e7a56953e56f@iupui.edu>', 12, 5173, '2007-02-23 15:19:58'))])
